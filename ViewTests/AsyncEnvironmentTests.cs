60472628d9139e6670015f9db4647f2e8f8c7cf8
Greg Liaskas <greg.liaskas@outlook.com>
2026-02-04 20:14:41 +0200

    new:: tests
    new tests for Asynchronous Environments added when corruption of View occurs in relation to the scope it is captured/accessed.

---
+++ ViewTests/AsyncEnvironmentTests.cs
@@ -0,0 +1,70 @@
+﻿using View;
+using ViewTests.Stubs;
+
+
+namespace ViewTests;
+
+public class AsyncEnvironmentTests
+{
+    [Fact]
+    public async Task UnmanagedTypes_Uncorrupted_When_CapturedScope_SameAs_AccessedScope()
+    {
+        var unmanagedStruct = new UnmanagedStruct(5, 8);
+        var view = View<UnmanagedStruct>.Of(ref unmanagedStruct);
+
+        await Task.Run(() => { Task.Delay(1000); });
+
+        var retrievedStruct = view.Read();
+        var result = Equals(retrievedStruct, unmanagedStruct);
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public async Task ManagedTypes_Uncorrupted_When_CapturedScope_SameAs_AccessedScope()
+    {
+        var managedStruct = new ManagedStruct(5, new StubClass(8));
+        var view = View<ManagedStruct>.Of(ref managedStruct);
+
+        await Task.Run(() => { Task.Delay(1000); });
+
+        var retrievedStruct = view.Read();
+        var result = Equals(retrievedStruct, managedStruct);
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public async Task UnmanagedTypes_Corrupted_When_CapturedScope_NarrowerThan_AccessedScope()
+    {
+        var escapedView = await Task.Run(() =>
+        {
+            var unmanagedStruct = new UnmanagedStruct(5, 8);
+            var view = View<UnmanagedStruct>.Of(ref unmanagedStruct);
+
+            return view;
+        });
+
+        var retrievedStruct = escapedView.Read();
+        var result = Equals(retrievedStruct, new UnmanagedStruct(5, 8));
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    public async Task ManagedTypes_Corrupted_When_CapturedScope_NarrowerThan_AccessedScope()
+    {
+        var escapedView = await Task.Run(() =>
+        {
+            var managedStruct = new ManagedStruct(5, new StubClass(8));
+            var view = View<ManagedStruct>.Of(ref managedStruct);
+
+            return view;
+        });
+
+        var retrievedStruct = escapedView.Read();
+        var result = Equals(retrievedStruct, new ManagedStruct(5, new StubClass(8)));
+
+        Assert.False(result);
+    }
+}
---
+++ ViewTests/Stubs/StubData.cs
@@ -0,0 +1,23 @@
+﻿namespace ViewTests.Stubs;
+
+internal class StubClass(int number)
+{
+    internal int Number { get; private set; } = number;
+
+}
+
+internal readonly struct UnmanagedStruct(int a, int b)
+{
+    internal int A { get; } = a;
+    internal int B { get; } = b;
+    
+    public static bool Equals(UnmanagedStruct x, UnmanagedStruct y) => x.A == y.A && x.B == y.B;
+}
+
+internal readonly struct ManagedStruct(int a, StubClass stubClass)
+{
+    internal int A { get; } = a;
+    internal StubClass Stub { get; } = stubClass;
+
+    public static bool Equals(ManagedStruct x, ManagedStruct y) => x.A == y.A && x.Stub.Number == y.Stub.Number;
+}
