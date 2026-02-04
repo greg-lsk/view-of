19347a1dc765d57ae55a8bb02af088675c7412f7
Greg Liaskas <greg.liaskas@outlook.com>
2026-02-04 17:29:30 +0200

    new:: View struct created

---
+++ View/View.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+
+
+namespace View;
+
+public unsafe readonly struct View<T> where T : struct
+{
+    private readonly void* _referedData;
+
+    private View(ref T referedData) => _referedData = Unsafe.AsPointer(ref referedData);
+    public static View<T> Of(ref T referedData) => new(ref referedData);
+
+    public readonly ref T Peek() => ref Unsafe.AsRef<T>(_referedData);
+    public readonly T Read() => Unsafe.Read<T>(_referedData);
+}
