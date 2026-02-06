ff6c219a8be29fb8587d0b64195bc4b7032cbbaa
Greg Liaskas <greg.liaskas@outlook.com>
2026-02-06 12:12:28 +0200

    deletion:: unnecessary sync test cases

--- ViewTests/SyncEnvironmentTests.cs
+++ ViewTests/SyncEnvironmentTests.cs
@@ -7,34 +7,6 @@ namespace ViewTests;
 public class SyncEnvironmentTests
 {
     [Fact]
-    public async Task UnmanagedTypes_Uncorrupted_When_CapturedScope_SameAs_AccessedScope()
-    {
-        var unmanagedStruct = new UnmanagedStruct(5, 8);
-        var view = View<UnmanagedStruct>.Of(ref unmanagedStruct);
-
-        StubExecution.SyncExecution();
-
-        var retrievedStruct = view.Read();
-        var result = Equals(retrievedStruct, unmanagedStruct);
-
-        Assert.True(result);
-    }
-
-    [Fact]
-    public async Task ManagedTypes_Uncorrupted_When_CapturedScope_SameAs_AccessedScope()
-    {
-        var managedStruct = new ManagedStruct(5, new StubClass(8));
-        var view = View<ManagedStruct>.Of(ref managedStruct);
-
-        StubExecution.SyncExecution();
-
-        var retrievedStruct = view.Read();
-        var result = Equals(retrievedStruct, managedStruct);
-
-        Assert.True(result);
-    }
-
-    [Fact]
     public async Task UnmanagedTypes_Corrupted_When_CapturedScope_NarrowerThan_AccessedScope()
     {
         (int a, int b) = (5, 8);
@@ -58,31 +30,5 @@ public class SyncEnvironmentTests
         var result = Equals(retrievedStruct, new ManagedStruct(a, new StubClass(b)));
 
         Assert.False(result);
-    }
-
-    [Fact]
-    public async Task UnmanagedTypes_Uncorrupted_When_PassedTo_And_HandledIn_SyncMethods()
-    {
-        var number = 5;
-        var unmanagedStruct = new UnmanagedStruct(number, 8);
-        var view = View<UnmanagedStruct>.Of(ref unmanagedStruct);
-
-        var retrievedNumber = StubExecution.SyncExecution(view);
-
-        Assert.Equal(retrievedNumber, number);
-    }
-
-    [Fact]
-    public async Task ManagedTypes_Uncorrupted_When_PassedTo_And_HandledIn_SyncMethods()
-    {
-        var stubClass = new StubClass(8);
-        var managedStruct = new ManagedStruct(5, stubClass);
-        var view = View<ManagedStruct>.Of(ref managedStruct);
-
-        var retrievedClass = StubExecution.SyncExecution(view);
-
-        var result = ReferenceEquals(stubClass, retrievedClass);
-
-        Assert.True(result);
     }
 }
