fd6e90646c5ad993f0d60e3a0b5f38ce362735e4
Greg Liaskas <greg.liaskas@outlook.com>
2026-02-06 11:16:46 +0200

    refactor:: removed the 'T is struct' constaint
    >doc:: also documentation was updated

--- View/View.cs
+++ View/View.cs
@@ -5,10 +5,10 @@ namespace View;
 
 /// <summary>
 /// Represents a view into a block of unmanaged memory that holds a value of a specified 
-/// struct type <typeparamref name="T"/>. This struct provides mechanisms to read data directly from the unmanaged memory.
+/// type <typeparamref name="T"/>. This struct provides mechanisms to read data directly from the unmanaged memory.
 /// </summary>
-/// <typeparam name="T">The type of the struct that this view points to. It must be a value type.</typeparam>
-public unsafe readonly struct View<T> where T : struct
+/// <typeparam name="T">The type that this view points to.</typeparam>
+public unsafe readonly struct View<T>
 {
     private readonly void* _referedData;
 
@@ -16,22 +16,22 @@ public unsafe readonly struct View<T> where T : struct
 
     /// <summary>
     /// Creates a new instance of the <see cref="View{T}"/> struct from a reference to 
-    /// the specified struct type <typeparamref name="T"/>.
+    /// the specified type <typeparamref name="T"/>.
     /// </summary>
     /// <param name="referedData">A reference to the data to be viewed.</param>
     /// <returns>A new <see cref="View{T}"/> instance that points to the specified data.</returns>
     public static View<T> Of(ref T referedData) => new(ref referedData);
 
     /// <summary>
-    /// Retrieves a read-only reference to the struct value located at the pointer position.
+    /// Retrieves a read-only reference to the type located at the pointer position.
     /// This allows for access to the data without modifying it.
     /// </summary>
-    /// <returns>A read-only reference to the struct value.</returns>
+    /// <returns>A read-only reference to <typeparamref name="T"/></returns>
     public readonly ref readonly T Peek() => ref Unsafe.AsRef<T>(_referedData);
 
     /// <summary>
-    /// Reads the struct value from the unmanaged memory directly and returns it.
+    /// Reads value from the unmanaged memory directly and returns it.
     /// </summary>
-    /// <returns>The struct value read from the unmanaged memory.</returns>
+    /// <returns>The value of <typeparamref name="T"/> read from the unmanaged memory.</returns>
     public readonly T Read() => Unsafe.Read<T>(_referedData);
 }
