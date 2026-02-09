ee8ab56c29c2e0f616ef24adb4cef76ef3144020
Greg Liaskas <greg.liaskas@outlook.com>
2026-02-09 14:28:34 +0200

    new:: Test cases for equality mechanism.

---
+++ ViewTests/EqualityTestsOnValueTypes.cs
@@ -0,0 +1,129 @@
+﻿using View;
+
+
+namespace ViewTests;
+ 
+public class EqualityTestsOnValueTypes
+{
+    [Fact]
+    internal void IEquatable_ReturnsTrue_WhenViewsAreOf_TheSameLocation()
+    {
+        var a = 5;
+        var view01 = View<int>.Of(ref a);
+        var view02 = View<int>.Of(ref a);
+
+        Assert.True(view01.Equals(view02));
+    }
+
+    [Fact]
+    internal void IEquatable_ReturnsFalse_WhenViewsAreOf_DifferentLocations()
+    {
+        var a = 5;
+        var view01 = View<int>.Of(ref a);
+
+        var b = 5;
+        var view02 = View<int>.Of(ref b);
+
+        Assert.False(view01.Equals(view02));
+    }
+
+    [Fact]
+    internal void Views_ToSameLocation_Have_TheSameHashCode()
+    {
+        var a = 5;
+
+        var hash01 = View<int>.Of(ref a).GetHashCode();
+        var hash02 = View<int>.Of(ref a).GetHashCode();
+
+        Assert.Equal(hash01, hash02);
+    }
+
+    [Fact]
+    internal void Views_ToDifferentLocations_Have_DifferentHashCodes()
+    {
+        var a = 5;
+        var hash01 = View<int>.Of(ref a).GetHashCode();
+
+        var b = 5;
+        var hash02 = View<int>.Of(ref b).GetHashCode();
+
+        Assert.NotEqual(hash01, hash02);
+    }
+
+    [Fact]
+    internal void ClassicEquals_ReturnsTrue_WhenViewsAreOf_TheSameLocation()
+    {
+        var a = 5;
+        var view01 = View<int>.Of(ref a);
+        var view02 = View<int>.Of(ref a) as object;
+
+        Assert.True(view01.Equals(view02));
+    }
+
+    [Fact]
+    internal void ClassicEquals_ReturnsFalse_WhenViewsAreOf_DifferentLocations()
+    {
+        var a = 5;
+        var view01 = View<int>.Of(ref a);
+
+        var b = 5;
+        var view02 = View<int>.Of(ref b) as object;
+
+        Assert.False(view01.Equals(view02));
+    }
+
+    [Fact]
+    internal void ClassicEquals_ReturnsFalse_When_ArgProvided_IsNotOfType_View()
+    {
+        var a = 5;
+        var view01 = View<int>.Of(ref a);
+
+        var b = 5;
+
+        Assert.False(view01.Equals(b));
+    }
+
+    [Fact]
+    internal void EqualityOperator_ReturnsTrue_WhenViewsAreOf_TheSameLocation()
+    {
+        var a = 5;
+        var view01 = View<int>.Of(ref a);
+        var view02 = View<int>.Of(ref a);
+
+        Assert.True(view01 == view02);
+    }
+
+    [Fact]
+    internal void EqualityOperator_ReturnsFalse_WhenViewsAreOf_DifferentLocations()
+    {
+        var a = 5;
+        var view01 = View<int>.Of(ref a);
+
+        var b = 5;
+        var view02 = View<int>.Of(ref b);
+
+        Assert.False(view01 == view02);
+    }
+
+    [Fact]
+    internal void InequalityOperator_ReturnsFalse_WhenViewsAreOf_TheSameLocation()
+    {
+        var a = 5;
+        var view01 = View<int>.Of(ref a);
+        var view02 = View<int>.Of(ref a);
+
+        Assert.False(view01 != view02);
+    }
+
+    [Fact]
+    internal void InequalityOperator_ReturnsTrue_WhenViewsAreOf_DifferentLocations()
+    {
+        var a = 5;
+        var view01 = View<int>.Of(ref a);
+
+        var b = 5;
+        var view02 = View<int>.Of(ref b);
+
+        Assert.True(view01 != view02);
+    }
+}
